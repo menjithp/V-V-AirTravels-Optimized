169ac19ba95bb2950f95dfce6d6448041df5f981
Menjith P <70083973+menjithp@users.noreply.github.com>
2023-09-17 22:14:04 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,7 @@ export default function Home() {
          month for migration and work visas.Over 50% of our customers are through word-of-mouth. No other company understands
           overseas careers like we do. " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
