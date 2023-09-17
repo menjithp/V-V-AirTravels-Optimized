757dfe6fd0254c2d9a2ad47a2ca7244a3d4eb04e
Menjith P <70083973+menjithp@users.noreply.github.com>
2023-09-17 22:04:01 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>V V AIR TRAVELS</title>
-        <meta name="description" content="V.V.AIR TRAVELS is India’s Best Overseas Career Consultant and 
-        largest B2C immigration firm. Established in 2020, We are Licensed Recruitment Agents in India . As part 
-        of our services, we provide personal one-on-one counseling to about 100+ individual inquiries every month 
-        for migration and work visas.Over 50% of
-         our customers are through word-of-mouth. No other company understands overseas careers like we do." />
+         <title>V V AIR TRAVELS | Foreign Job Consultancy | India's Best Immigration Agency</title>
+        <meta name="description" content="V.V.Air Travels. India's Best Immigration Consultant.V.V.Air Travels is India’s Best
+        foreign job consultancy and overseas job agency. Established in 2020, We are Licensed foreign Recruitment Agency 
+         in India . As part of our services, we provide personal one-on-one counseling to about 100+ individual inquiries every 
+         month for migration and work visas.Over 50% of our customers are through word-of-mouth. No other company understands
+          overseas careers like we do. " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
