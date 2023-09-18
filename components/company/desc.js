5c65b1c6fab8eaf81bb9bd5fff9c036611dd45e3
Menjith P <70083973+menjithp@users.noreply.github.com>
2023-09-18 10:11:50 +0530

    Add files via upload

--- components/company/desc.js
+++ components/company/desc.js
@@ -15,8 +15,8 @@ export default ()=>{
     return <section className="">
         <article>
             <h3 className="subheadings text-center fw-bold violet-color">Want to Work abroad?</h3>
-            <p className="letter-spacing-desc"> <strong className="fw-normal"><h5 className="fs-5 d-inline fw-bolder">{state.Name}</h5>
-          {state.companyDesc}</strong> </p>
+            <p className="letter-spacing-desc"> <strong className="fs-5 d-inline fw-bolder">{state.Name}</strong>
+            <strong className="fw-normal">{state.companyDesc}</strong> </p>
 
             <div className="d-flex gap-4 align-items-center">
                 <a href={state.Facebook}><BsFacebook  className="facebook" size={25} /></a>
